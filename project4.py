f10d54a5fcdbd8e70bb88f1f6465438e3e058941
yeetdream <savenkovkaur@gmail.com>
2023-05-23 15:34:53 +0300

    First commit from Savenkov

---
+++ project4.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+def parse_books(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    books = []
+    for book in soup.find_all('li', class_='col-xs-6 col-sm-4 col-md-3 col-lg-3'):
+        title = book.h3.a['title']
+        price = book.find('p', class_='price_color').text
+        books.append((title, price[1:]))
+    return books
+
+url = 'http://books.toscrape.com/catalogue/category/books/classics_6/index.html'
+print(parse_books(url))
